9f8fb1cc9ade136f48fc4199e1eabe1952241663
yordawira <50675414+yordawira@users.noreply.github.com>
2022-09-25 15:18:48 +0700

    repair logic

--- app/src/main/java/com/coba/chalangechapter_4wave_15/enum/PlayerPosition.kt
+++
@@ -1,6 +0,0 @@
-package com.coba.chalangechapter_4wave_15.enum
-
-class PlayerPosition {
-    RIGHT,
-    LEFT
-}
---
+++ app/src/main/java/com/coba/chalangechapter_4wave_15/manager/CallBack.kt
@@ -0,0 +1,6 @@
+package com.coba.chalangechapter_4wave_15.manager
+
+interface CallBack {
+    fun kirimBalik(hasil: Int)
+    fun kirimLagi(computer: String)
+}
